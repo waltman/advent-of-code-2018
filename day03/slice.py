1a85bfac82d3c54bda7f7c1387d951b6b80acadd
Walt Mankowski (macbook pro) <waltman@pobox.com>
2018-12-03 06:49:17 -0500

    added code to solve part 2 in python

--- day03/slice.py
+++ day03/slice.py
@@ -6,6 +6,7 @@ class Fabric:
     def __init__(self, size):
         self.size = size
         self.grid = [[0 for col in range(size)] for row in range(size)]
+        self.claims = {}
 
     def __str__(self):
         s = ''
@@ -15,7 +16,8 @@ class Fabric:
             s += '\n'
         return s
 
-    def add_claim(self, coff, roff, w, h):
+    def add_claim(self, id, coff, roff, w, h):
+        self.claims[id] = (coff, roff, w, h)
         for col in range(coff, coff+w):
             for row in range(roff, roff+h):
                 self.grid[row][col] += 1
@@ -28,18 +30,32 @@ class Fabric:
                     cnt += 1
         return cnt
 
+    def is_intact(self, coff, roff, w, h):
+        for col in range(coff, coff+w):
+            for row in range(roff, roff+h):
+                if self.grid[row][col] != 1:
+                    return False
+        return True
+
+    def find_intact_claim(self):
+        for claim in self.claims:
+            (coff, roff, w, h) = self.claims[claim]
+            if self.is_intact(coff, roff, w, h):
+                print(f'claim {claim} is intact')
+
+
 fabric = Fabric(1000)
 filename = argv[1]
 with open(filename) as f:
     for line in f:
         m =re.match('#(\d+) @ (\d+),(\d+): (\d+)x(\d+)', line)
+        id = m.group(1)
         coff = int(m.group(2))
         roff = int(m.group(3))
         w = int(m.group(4))
         h = int(m.group(5))
-        fabric.add_claim(coff, roff, w, h)
+        fabric.add_claim(id, coff, roff, w, h)
 
 print(fabric.multi_claims())
 
-
-        
+fabric.find_intact_claim()
