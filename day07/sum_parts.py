0e019d0fb5728d68b3cbdaec9b856768fa81b9ca
Walt Mankowski <waltman@pobox.com>
2018-12-07 16:40:10 -0500

    python code for day 7 part 1

---
+++ day07/sum_parts.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+from sys import argv
+from collections import defaultdict
+
+remaining = set()
+done = set()
+parents = defaultdict(set)
+children = defaultdict(set)
+
+filename = argv[1]
+with open(filename) as f:
+    for line in f:
+        step1 = line[5]
+        step2 = line[36]
+        remaining.add(step1)
+        remaining.add(step2)
+        children[step1].add(step2)
+        parents[step2].add(step1)
+
+# initialize queue with root
+queue = set()
+for step in remaining:
+    if len(parents[step]) == 0:
+        queue.add(step)
+
+res1 = ''
+while queue:
+    # which to do next?
+    for step in sorted(queue):
+        if len(parents[step] & remaining) == 0:
+            break
+
+    # ok, we're picking step
+    queue.remove(step)
+    remaining.remove(step)
+    res1 += step
+    for child in children[step]:
+        if child in remaining:
+            queue.add(child)
+
+print('step1:', res1)
+
