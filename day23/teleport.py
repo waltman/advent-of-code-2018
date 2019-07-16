4bf97c9eab53df4f51e979b5eda9b5f59ebef763
Walt Mankowski <waltman@pobox.com>
2019-07-16 10:19:37 -0400

    added l1_dist() and in_range_p()

--- day23/teleport.py
+++ day23/teleport.py
@@ -2,6 +2,12 @@
 from sys import argv
 import re
 
+def l1_dist(p1, p2):
+    return abs(p1[0]-p2[0]) + abs(p1[1]-p2[1]) + abs(p1[2]-p2[2])
+
+def in_range_p(p1, p2, r):
+    return l1_dist(p1, p2) <= r
+
 nanobot_pos = []
 nanobot_r = []
 filename = argv[1]
@@ -12,7 +18,7 @@ with open(filename) as f:
         y = int(m.group(2))
         z = int(m.group(3))
         r = int(m.group(4))
-        nanobot_pos.append([x, y, z])
+        nanobot_pos.append((x, y, z))
         nanobot_r.append(r)
 
 best_r = -1
@@ -25,9 +31,7 @@ for idx in range(len(nanobot_r)):
 in_range = 0
 bx,by,bz = nanobot_pos[best_idx]
 for nb_pos in nanobot_pos:
-    x,y,z = nb_pos
-    if abs(bx-x) + abs(by-y) + abs(bz-z) <= best_r:
+    if l1_dist((bx,by,bz), (nb_pos)) <= best_r:
         in_range += 1
 
 print('part1', in_range)
-
